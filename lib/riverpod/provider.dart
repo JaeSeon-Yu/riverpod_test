67225d3b02bb25a5c0b13208c00582b72b811559
JaeSeon-Yu <stacey5165@naver.com>
2022-11-04 17:43:17 +0900

    + provider in provider

---
+++ lib/riverpod/provider.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_test/model/shopping_item_model.dart';
+import 'package:riverpod_test/riverpod/state_notifier_provider.dart';
+
+final filteredShoppingListProvider = Provider<List<ShoppingItemModel>>((ref) {
+  final filterState = ref.watch(filterProvider);
+  final shoppingListState = ref.watch(shoppingListProvider);
+
+  switch (filterState) {
+    case FilterState.all:
+      return shoppingListState;
+
+    case FilterState.spicy:
+      return shoppingListState.where((element) => element.isSpicy).toList();
+
+    case FilterState.notSpicy:
+      return shoppingListState.where((element) => !element.isSpicy).toList();
+  }
+});
+
+enum FilterState {
+  notSpicy,
+  spicy,
+  all,
+}
+
+final filterProvider = StateProvider<FilterState>((ref) => FilterState.all);
